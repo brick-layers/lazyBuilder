ca5d90c3a5f2473f4f5ae78a0c5ffff31f233cf8
Evans Stepanov <evans@stepanov.email>
2017-11-10 17:41:44 -0500

    drag and drop done

--- src/components/Drag/Drag.js
+++ src/components/Drag/Drag.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import lazySQL from 'lazysql'
 
 import { actions } from './component.js'
 import styles from './component.less'
@@ -20,6 +21,9 @@ const mapDispatchToProps = dispatch => {
     },
     dragInSetOutputPath: source => {
       dispatch(actions.setOutputPath(source))
+    },
+    setComplete: () => {
+      dispatch(actions.completeLay({ complete: true }))
     }
   }
 }
@@ -48,13 +52,15 @@ class Drag extends Component {
           <img width="60%" src={logoimg} alt="lazyBuilder Logo" />
           <div className={`${styles.logoText}`}>lazyBuilder</div>
         </div>
-        {!this.props.inputFile && (
-          <div className={`${styles.center} ${styles.fill}`}>
-            <h3 className={styles.dragText}>Drop a JSON file here</h3>
-            <h1 />
-          </div>
-        )}
+        {!this.props.inputFile &&
+          !this.props.complete && (
+            <div className={`${styles.center} ${styles.fill}`}>
+              <h3 className={styles.dragText}>Drop a JSON file here</h3>
+              <h1 />
+            </div>
+          )}
         {this.props.inputFile &&
+          !this.props.complete &&
           !this.props.outputPath && (
             <div className={`${styles.center} ${styles.fill}`}>
               <h3 className={styles.dragText}>{this.props.inputFile}</h3>
@@ -73,22 +79,40 @@ class Drag extends Component {
                       console.log(this.props)
                       this.props.dragInSetOutputPath({ outputPath: path[0] })
                     }
-                  )}
+                  )
+                }
               >
                 Select Directory
               </button>
             </div>
           )}
         {this.props.inputFile &&
+          !this.props.complete &&
           this.props.outputPath && (
             <div className={`${styles.center} ${styles.fill}`}>
               <h3 className={styles.dragText}>input file</h3>
               <p>{this.props.inputFile}</p>
               <h3 className={styles.dragText}>output path</h3>
               <p>{this.props.outputPath}</p>
-              <button>Make Some Magic</button>
+              <button
+                onClick={() => {
+                  lazySQL(this.props.inputFile, this.props.outputPath)
+                  this.props.setComplete()
+                }}
+              >
+                Make Some Magic
+              </button>
             </div>
           )}
+        {this.props.complete && (
+          <div className={`${styles.center} ${styles.fill}`}>
+            <h3 className={styles.dragText}>
+              Do a little dance,<br />make a little code,<br />and get ready
+              because your files are ready!
+            </h3>
+            <h1 />
+          </div>
+        )}
       </div>
     )
   }
--- src/components/Drag/component.js
+++ src/components/Drag/component.js
@@ -9,34 +9,41 @@ import component from './Drag.js'
 const DUMMY_ACTION = reduxUtil.defineAction('DUMMY_ACTION')
 const SET_INPUT = reduxUtil.defineAction('SET_INPUT')
 const SET_OUTPUT = reduxUtil.defineAction('SET_OUTPUT')
+const COMPLETE = reduxUtil.defineAction('COMPLETE')
 
 // Initial State
 const initialState = {
   dummyState: false,
   inputFile: null,
-  outputPath: null
+  outputPath: null,
+  complete: false
 }
 
 // Make Actions
 const actions = {
   dummyAction: reduxUtil.createAction(DUMMY_ACTION),
   setInputFile: reduxUtil.createAction(SET_INPUT),
-  setOutputPath: reduxUtil.createAction(SET_OUTPUT)
+  setOutputPath: reduxUtil.createAction(SET_OUTPUT),
+  completeLay: reduxUtil.createAction(COMPLETE)
 }
 
 // Make reducer
 const reducer = reduxUtil.createReducer(
   {
-    [DUMMY_ACTION]: function (state, action) {
+    [DUMMY_ACTION]: function(state, action) {
       let newState = { ...state, ...action.payload }
       newState.dummyState = true
       return newState
     },
-    [SET_OUTPUT]: function (state, action) {
+    [SET_OUTPUT]: function(state, action) {
       let newState = { ...state, ...action.payload }
       return newState
     },
-    [SET_INPUT]: function (state, action) {
+    [SET_INPUT]: function(state, action) {
+      let newState = { ...state, ...action.payload }
+      return newState
+    },
+    [COMPLETE]: function(state, action) {
       let newState = { ...state, ...action.payload }
       return newState
     }
