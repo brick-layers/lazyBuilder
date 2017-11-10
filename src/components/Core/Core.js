6e8cb60173e18c6ae1a2e1eda55f736b5c383076
Evans Stepanov <evans@stepanov.email>
2017-11-10 12:46:38 -0500

    home page drop

--- src/components/Core/Core.js
+++ src/components/Core/Core.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { Route, Switch } from 'react-router-dom';
-import { ConnectedRouter } from 'connected-react-router';
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { Route, Switch } from 'react-router-dom'
+import { ConnectedRouter } from 'connected-react-router'
 
-import { components, history, store } from '../components.js';
-import styles from './component.less';
+import { components, history, store } from '../components.js'
+import styles from './component.less'
+import logoimg from '../../assets/lazybuilder.png'
+
+document.ondragover = document.ondrop = ev => {
+  ev.preventDefault()
+}
+
+document.body.ondrop = ev => {
+  ev.preventDefault()
+  // this.setState({ json: ev.dataTransfer.files[0].path })
+  console.log(ev.dataTransfer.files[0].path)
+}
 
 const Core = () => {
   return (
@@ -13,15 +24,19 @@ const Core = () => {
         <div className="window">
           <div className="window-content">
             <div className="pane-group">
-              <div className="pane-sm sidebar"><components.Menu /></div>
-              <div className="pane padded"><AppRouter /></div>
+              <div className="pane-sm sidebar">
+                <components.Menu />
+              </div>
+              <div className="pane padded">
+                <AppRouter />
+              </div>
             </div>
           </div>
           <components.Footer />
         </div>
       </ConnectedRouter>
     </Provider>
-  );
+  )
 }
 
 const AppRouter = () => {
@@ -30,21 +45,26 @@ const AppRouter = () => {
       <Route exact path="/" component={Home} />
       <Route path="/example" component={components.Example} />
     </Switch>
-  );
+  )
 }
 
 const Home = () => {
   return (
     <div>
-      <h1>Hello, Electron!</h1>
-      <p>I hope you enjoy using enhanced-electron-react-boilerplate to start your dev off right!</p>
-      <div className='padded'>
-        <div className={`box padded ${styles.box}`}>
-          This has a different background color, but uses the same 'box' className. However, thanks to CSS modules the names dont collide. Here we are setting a background color, and overriding the shadow.
-        </div>
+      <div className={styles.center}>
+        <img width="50%" src={logoimg} alt="lazyBuilder Logo" />
+        <div className={`${styles.logoText}`}>lazyBuilder</div>
+        <h3 className={styles.dragText}>Drop a JSON file here</h3>
       </div>
+      {/*<div className="padded">
+        <div className={`box padded ${styles.box}`}>
+          This has a disdsdsdsdfferent background color, but uses the same 'box'
+          className. However, thanks to CSS modules the names dont collide. Here
+          we are setting a background color, and overriding the shadow.
+        </div>
+  </div>*/}
     </div>
-  );
+  )
 }
 
-export default Core;
+export default Core
