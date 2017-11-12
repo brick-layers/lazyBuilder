10bb74a2fa9a72a0f0bacfcc1ed7f69e39c366c6
Evans Stepanov <evans@stepanov.email>
2017-11-12 10:57:59 -0500

    scaffold for output component

---
+++ src/components/OutputFile/OutputFile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class OutputFile extends Component {
+  constructor( props ) {
+    super( props );
+  }
+  render() {
+    return (
+      <div>OutputFile -- Stateful</div>
+    );
+  }
+}
+
+export default OutputFile;
